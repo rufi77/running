44049dac7778fda037b7eb10d05f2b562bc9722f
Rufibach <rufiback@emea.roche.com>
2024-02-18 15:30:38 +0100

    update

--- functions/hm_print.r
+++ functions/hm_print.r
@@ -1,7 +1,7 @@
 hm_print <- function(dat, cut = 16){
   
   t <- (dat 
-  %>% select(start, end, date, hm_diff, time, hm_h, begleitung1) 
+  %>% select(start, end, date, hm_diff, time, hm_h, beg) 
   %>% arrange(desc(hm_h)) 
   %>% mutate(start = substr(start, 1, cut), end = substr(end, 1, cut)))
 
